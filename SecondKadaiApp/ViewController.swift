69a3242d1edc090f34a749ec95a393ee2994bb5c
daisuke.kokubu <k.daidaidai0826@gmail.com>
2020-10-14 12:06:14 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 國分大輔 on 2020/10/14.
+//  Copyright © 2020 daisuke.kokubu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
